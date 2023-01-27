a5c0be65896b68776c7ebbe323e440cccf7f02da
faithgaiciumia <faithgaiciumia078@gmail.com>
2023-01-27 14:53:31 +0300

    home sidebar and navigation bar

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,7 +1,29 @@
-export default function Home(){
-    return(
-        <div>
-            <h1>You are home.</h1>
+import SideBar from "../../components/SideBar";
+import NavBar from "../../components/NavBar";
+import { createUseStyles } from "react-jss";
+
+export default function Home() {
+  const styles = useStyles();
+  return (
+    <>
+      <NavBar />
+      <div className={styles.row}>
+        <SideBar />
+        <div className={styles.col}>
+          <h1>main</h1>
         </div>
-    )
+      </div>
+    </>
+  );
 }
+
+const useStyles = createUseStyles({
+  row: {
+    display: "flex",
+    alignContent: "center",
+    alignItems: "center",
+  },
+  col: {
+    width: "70%",
+  },
+});
